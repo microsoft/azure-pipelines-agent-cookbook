73026811bab369ea2492f1f0483edfd0e2c4dcb2
Dmitry Ivanov <iva9im@gmail.com>
2017-12-27 23:59:20 -0800

    Update URLs to the new format

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,12 +1,12 @@
-default['vsts_agent']['binary']['version'] = '2.125.1'
+default['vsts_agent']['binary']['version'] = '2.126.0'
 
 case node['platform_family']
 when 'windows'
-  default['vsts_agent']['binary']['url'] = 'https://github.com/Microsoft/vsts-agent/releases/download/v%s/vsts-agent-win-x64-%s.zip'
+  default['vsts_agent']['binary']['url'] = 'https://vstsagentpackage.azureedge.net/agent/%s/vsts-agent-win-x64-%s.zip'
 when 'debian', 'rhel'
-  default['vsts_agent']['binary']['url'] = 'https://github.com/Microsoft/vsts-agent/releases/download/v%s/vsts-agent-linux-x64-%s.tar.gz'
+  default['vsts_agent']['binary']['url'] = 'https://vstsagentpackage.azureedge.net/agent/%s/vsts-agent-linux-x64-%s.tar.gz'
 when 'mac_os_x', 'mac_os_x_server'
-  default['vsts_agent']['binary']['url'] = 'https://github.com/Microsoft/vsts-agent/releases/download/v%s/vsts-agent-osx-x64-%s.tar.gz'
+  default['vsts_agent']['binary']['url'] = 'https://vstsagentpackage.azureedge.net/agent/%s/vsts-agent-osx-x64-%s.tar.gz'
 end
 
 # applies for debian based distros: ubuntu, debian etc...
