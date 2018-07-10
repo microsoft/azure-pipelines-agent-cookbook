084e7005ef4e1069a8fa54db2b3b049af67943d4
ivadim <iva9im@gmail.com>
2018-07-09 22:58:06 -0700

    Be able to use any version of seven_zip greater or equal to 2.0

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url       'https://github.com/Microsoft/vsts-agent-cookbook' if respond_to?(:source_url)
 issues_url       'https://github.com/Microsoft/vsts-agent-cookbook/issues' if respond_to?(:issues_url)
 chef_version     '>= 12.4' if respond_to?(:chef_version)
-version          '2.0.2'
+version          '2.0.3'
 
 %w(ubuntu debian redhat centos mac_os_x windows).each do |operating_system|
   supports operating_system
