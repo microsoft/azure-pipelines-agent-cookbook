07ff4cc7eba653f90b00d58afb15d74f14c4eb63
corey.hemminger <corey.hemminger@protolabs.com>
2018-07-30 17:06:45 -0500

    updated for chef 14+ support

--- libraries/service.rb
+++ libraries/service.rb
@@ -1,7 +1,8 @@
 require 'chef/resource/lwrp_base'
 require 'chef/provider/lwrp_base'
 require 'chef/mixin/shell_out'
-require 'chef/mixin/language'
+require 'chef/dsl/platform_introspection'
+
 
 module VSTS
   module Agent
