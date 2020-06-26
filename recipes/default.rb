bd13af17969438284f6b3702fd08eb4e6e1cde26
Eammon Hanlon <eammon.hanlon@microsoft.com>
2020-06-26 10:03:13 -0700

    Update libssl version for debian-10 and change liburl to curl

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,11 +1,11 @@
 
 if platform_family?('debian') && node['vsts_agent']['prerequisites']['debian']['install']
 
-  %w(libunwind8 liblttng-ust0 libcurl3 libuuid1 libkrb5-3 zlib1g).each do |pkg|
+  %w(libunwind8 liblttng-ust0 curl libuuid1 libkrb5-3 zlib1g).each do |pkg|
     package pkg
   end
 
-  package 'libssl1.0.0' unless node['platform_version'] =~ /^9.*/
+  package 'libssl1.1' if node['platform_version'] =~ /^10.*/
   package 'libssl1.0.2' if node['platform_version'] =~ /^9.*/
   package 'libicu52' if node['platform_version'] =~ /^14.*/ || node['platform_version'] =~ /^8.*/
   package 'libicu55' if node['platform_version'] =~ /^16.*/
