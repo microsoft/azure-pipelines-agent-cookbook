6549a15e9df00a934156cb5dda9823cc87ea144a
Eammon Hanlon <eammon.hanlon@microsoft.com>
2020-06-27 09:01:27 -0700

    Update attribute root key to azure_pipelines_agent and updated default agent version to latest

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,16 +1,13 @@
-default['vsts_agent']['binary']['version'] = '2.136.1'
+default['azure_pipelines_agent']['binary']['version'] = '2.170.1'
 
 case node['platform_family']
 when 'windows'
-  default['vsts_agent']['binary']['url'] = 'https://vstsagentpackage.azureedge.net/agent/%s/vsts-agent-win-x64-%s.zip'
+  default['azure_pipelines_agent']['binary']['url'] = 'https://vstsagentpackage.azureedge.net/agent/%s/vsts-agent-win-x64-%s.zip'
 when 'debian', 'rhel'
-  default['vsts_agent']['binary']['url'] = 'https://vstsagentpackage.azureedge.net/agent/%s/vsts-agent-linux-x64-%s.tar.gz'
+  default['azure_pipelines_agent']['binary']['url'] = 'https://vstsagentpackage.azureedge.net/agent/%s/vsts-agent-linux-x64-%s.tar.gz'
 when 'mac_os_x'
-  default['vsts_agent']['binary']['url'] = 'https://vstsagentpackage.azureedge.net/agent/%s/vsts-agent-osx-x64-%s.tar.gz'
+  default['azure_pipelines_agent']['binary']['url'] = 'https://vstsagentpackage.azureedge.net/agent/%s/vsts-agent-osx-x64-%s.tar.gz'
 end
 
-# applies for debian based distros: ubuntu, debian etc...
-default['vsts_agent']['prerequisites']['debian']['install'] = true
-
-# applies for redhat based distros: redhat, centos etc...
-default['vsts_agent']['prerequisites']['redhat']['install'] = true
+default['azure_pipelines_agent']['prerequisites']['debian']['install'] = true
+default['azure_pipelines_agent']['prerequisites']['redhat']['install'] = true
