fe4e93a082b2495feb4d407d0bca092ae28aa474
Eammon Hanlon <eammon.hanlon@microsoft.com>
2020-06-28 12:09:31 -0700

    Set default attributes for ChefSpec tests

--- spec/resources/azure_pipelines_agent_spec.rb
+++ spec/resources/azure_pipelines_agent_spec.rb
@@ -1,8 +1,12 @@
 require 'spec_helper'
 
-describe 'installing azure pipelines agent with the default action' do
+describe 'installing azure pipelines agent with the default action on ubuntu' do
   step_into :azure_pipelines_agent
   platform 'ubuntu'
+
+  default_attributes['azure_pipelines_agent']['binary']['version'] = '2.170.1'
+  default_attributes['azure_pipelines_agent']['binary']['url'] = 'https://vstsagentpackage.azureedge.net/agent/%s/vsts-agent-linux-x64-%s.tar.gz'
+
   context 'with the default action' do
     recipe do
       azure_pipelines_agent 'node1' do
